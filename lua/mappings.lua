88cc69d487b86496a35d3f9872057ff5eb5e4a79
yangjianwu <yangjianwu@hotmail.com>
2025-08-22 17:56:18 +0800

    float term

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,8 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
 
+vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
+
 
 
 vim.keymap.set("n", "<leader>fm", function()
