9c9868295fe691f141fd97a881345d2f005e1440
yangjianwu <yangjianwu@hotmail.com>
2025-08-28 18:03:26 +0800

    change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,6 +25,49 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.lsp.buf.format({ async = true })
     end, { buffer = event.buf, desc = "LSP: Format current buffer" })
 
+
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    -- Rename the variable under your cursor.
+    --  Most Language Servers support renaming across files, etc.
+    map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+    -- Execute a code action, usually your cursor needs to be on top of an error
+    -- or a suggestion from your LSP for this to activate.
+    map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+    -- Find references for the word under your cursor.
+    map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+    -- Jump to the implementation of the word under your cursor.
+    --  Useful when your language has ways of declaring types without an actual implementation.
+    map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+    -- Jump to the definition of the word under your cursor.
+    --  This is where a variable was first declared, or where a function is defined, etc.
+    --  To jump back, press <C-t>.
+    map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+    -- WARN: This is not Goto Definition, this is Goto Declaration.
+    --  For example, in C this would take you to the header.
+    map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+    -- Fuzzy find all the symbols in your current document.
+    --  Symbols are things like variables, functions, types, etc.
+    map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+
+    -- Fuzzy find all the symbols in your current workspace.
+    --  Similar to document symbols, except searches over your entire project.
+    map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+
+    -- Jump to the type of the word under your cursor.
+    --  Useful when you're not sure what type a variable is and you want to see
+    --  the definition of its *type*, not where it was *defined*.
+    map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+
     vim.api.nvim_create_user_command(
       "LspInfo",
       ":checkhealth vim.lsp",
@@ -37,6 +80,79 @@ vim.api.nvim_create_autocmd("LspAttach", {
       desc = "Opens the Nvim LSP client log.",
     })
 
+    vim.api.nvim_create_user_command("CleanJavaWorkSpace", function()
+      local classpath = vim.fn.getcwd() .. "/.classpath"
+      if vim.fn.filereadable(classpath) == 1 then
+        os.remove(classpath)
+        vim.notify(".classpath 已删除", vim.log.levels.INFO)
+      else
+        vim.notify("未发现 .classpath 文件", vim.log.levels.WARN)
+      end
+
+      -- 重启 LSP（需要 nvim-lspconfig 0.8+）
+      vim.lsp.stop_client(client.id)
+      vim.cmd("edit") -- 重新加载当前 buffer 以触发 lsp 重启
+    end, { desc = "清理Java环境" })
+
     vim.lsp.inlay_hint.enable(true)
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("LspProgress", {
+--   ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+--   callback = function(ev)
+--     local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+--     vim.notify(vim.lsp.status(), "info", {
+--       id = "lsp_progress",
+--       title = "LSP Progress",
+--       opts = function(notif)
+--         notif.icon = ev.data.params.value.kind == "end" and " "
+--             or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+--       end,
+--     })
+--   end,
+-- })
+
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local value = ev.data.params
+    .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+    if not client or type(value) ~= "table" then
+      return
+    end
+    local p = progress[client.id]
+
+    for i = 1, #p + 1 do
+      if i == #p + 1 or p[i].token == ev.data.params.token then
+        p[i] = {
+          token = ev.data.params.token,
+          msg = ("[%3d%%] %s%s"):format(
+            value.kind == "end" and 100 or value.percentage or 100,
+            value.title or "",
+            value.message and (" **%s**"):format(value.message) or ""
+          ),
+          done = value.kind == "end",
+        }
+        break
+      end
+    end
+
+    local msg = {} ---@type string[]
+    progress[client.id] = vim.tbl_filter(function(v)
+      return table.insert(msg, v.msg) or not v.done
+    end, p)
+
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(table.concat(msg, "\n"), "info", {
+      id = "lsp_progress",
+      title = client.name,
+      opts = function(notif)
+        notif.icon = #progress[client.id] == 0 and " "
+            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
   end,
 })
--- lua/plugins/notice.lua
+++ lua/plugins/notice.lua
@@ -1,55 +1,55 @@
 return {
   -- lazy.nvim
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          lsp_doc_border = true,        -- add a border to hover docs and signature help
-        },
-      })
-
-      vim.keymap.set("n", "<leader>nh", "<cmd>Noice history<CR>", { desc = "Noice history" })
-      vim.keymap.set("n", "<leader>nl", "<cmd>Noice last<CR>", { desc = "Noice last message" })
-
-      require("notify").setup({
-        background_colour = "#000000",
-      })
-    end,
-  },
-  {
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    config = function()
-      local notify = require("notify")
-      notify.setup({
-        stages = "fade_in_slide_out", -- 动画: 渐入滑出
-        timeout = 3000,               -- 自动关闭时间(ms)
-        background_colour = "#000000",
-        render = "default",           -- 也可以 "minimal" / "compact"
-        max_width = 80,
-        max_height = 20,
-      })
-      vim.notify = notify
-    end,
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --   },
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "rcarriga/nvim-notify",
+  --   },
+  --   config = function()
+  --     require("noice").setup({
+  --       lsp = {
+  --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --         override = {
+  --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --           ["vim.lsp.util.stylize_markdown"] = true,
+  --           -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+  --         },
+  --       },
+  --       -- you can enable a preset for easier configuration
+  --       presets = {
+  --         bottom_search = true,         -- use a classic bottom cmdline for search
+  --         command_palette = true,       -- position the cmdline and popupmenu together
+  --         long_message_to_split = true, -- long messages will be sent to a split
+  --         lsp_doc_border = true,        -- add a border to hover docs and signature help
+  --       },
+  --     })
+  --
+  --     vim.keymap.set("n", "<leader>nh", "<cmd>Noice history<CR>", { desc = "Noice history" })
+  --     vim.keymap.set("n", "<leader>nl", "<cmd>Noice last<CR>", { desc = "Noice last message" })
+  --
+  --     require("notify").setup({
+  --       background_colour = "#000000",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "rcarriga/nvim-notify",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     local notify = require("notify")
+  --     notify.setup({
+  --       stages = "fade_in_slide_out", -- 动画: 渐入滑出
+  --       timeout = 3000,               -- 自动关闭时间(ms)
+  --       background_colour = "#000000",
+  --       render = "default",           -- 也可以 "minimal" / "compact"
+  --       max_width = 80,
+  --       max_height = 20,
+  --     })
+  --     vim.notify = notify
+  --   end,
+  -- },
 }
