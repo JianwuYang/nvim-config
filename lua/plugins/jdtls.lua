f59081206dd51098acd546bad1f649ceeb136b2b
yangjianwu <yangjianwu@hotmail.com>
2025-09-01 16:56:08 +0800

    change

---
+++ lua/plugins/jdtls.lua
@@ -0,0 +1,46 @@
+return {
+  "mfussenegger/nvim-jdtls",
+  config = function()
+    local eclipse_jdtls_path = vim.fn.expand("$MASON/packages/jdtls")
+    local equinox_launcher_path = vim.fn.glob(eclipse_jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar", 1)
+    local lombok = eclipse_jdtls_path .. "/lombok.jar"
+    local config_linux = eclipse_jdtls_path .. "/config_linux"
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local home = os.getenv("HOME")
+    local workspace_dir = home .. "/.cache/jdtls/workspace/" .. project_name
+
+    local config = {
+      cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.level=ALL",
+        "-Xmx1G",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-javaagent:" .. lombok,
+        "-jar",
+        equinox_launcher_path,
+        "-configuration",
+        config_linux,
+        "-data",
+        workspace_dir,
+      },
+      root_dir = vim.fs.root(0, {
+        ".git",
+        "build.gradle",
+        "build.gradle.kts",
+        "build.xml",
+        "pom.xml",
+        "settings.gradle",
+        "settings.gradle.kts",
+      })
+    }
+
+    vim.lsp.config("jdtls", config)
+  end
+}
