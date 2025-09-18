e994e0a7c7d4f0a3dac15a0738a724bde1368cd2
yangjianwu <yangjianwu@hotmail.com>
2025-09-18 15:16:44 +0800

    change

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,18 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
-	},
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local npairs = require("nvim-autopairs")
+      local Rule   = require("nvim-autopairs.rule")
+
+      npairs.setup({
+        check_ts = true,
+      })
+
+      npairs.add_rules({
+        Rule("/*", "/", "java"),
+      })
+    end
+  },
 }
