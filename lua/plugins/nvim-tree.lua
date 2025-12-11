14a83126158c8046a50d72da97a23f5fa12beb7c
yangjianwu <yangjianwu@hotmail.com>
2025-12-11 18:02:31 +0800

    change

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,7 +6,7 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {
+    require("nvim-tree").setup({
       sort = {
         sorter = "case_sensitive",
       },
@@ -14,12 +14,36 @@ return {
         width = 30,
       },
       renderer = {
+        highlight_git = "all",
+        highlight_diagnostics = "all",
+        highlight_opened_files = "all",
+        highlight_modified = "all",
+        highlight_hidden = "all",
+        indent_width = 1,
         group_empty = true,
+        hidden_display = "simple",
+        icons = {
+          git_placement = "after",
+        }
       },
       filters = {
-        dotfiles = true,
+        dotfiles = false,
+        custom = { "^.git$" },
+        exclude = { "application", ".vscode" },
       },
-    }
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+      },
+      modified = {
+        enable = true,
+      },
+      update_focused_file = {
+        enable = true,
+      },
+    })
+
+    -- 推荐绑定快捷键（<leader>e 打开/关闭树）
     vim.keymap.set("n", "<c-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
   end,
 }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "master",
+    -- or                              , branch = '0.1.x',
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          path_display = { "smart" }
+        },
+        pickers = {
+          oldfiles = {
+            cwd_only = true,
+          },
+          find_files = {
+            find_command = { "fd" }
+          },
+        },
+      })
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fa", function()
+        builtin.find_files({ find_command = { "fd", "-I" } })
+      end, { desc = "Find all files (including hidden)" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope " })
+
+
+      vim.api.nvim_create_user_command("Dotfiles", function()
+        require("telescope.builtin").find_files({
+          prompt_title = "Dotfiles",
+          cwd = "~/.config/nvim", -- 这里改成你 dotfiles 的路径
+          hidden = true,
+        })
+      end, {})
+    end,
+  },
+}
