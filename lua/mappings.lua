fa32f03ec6f5c5ceef7d9b6a892607d22a9a4e46
yangjianwu <yangjianwu@hotmail.com>
2025-12-15 10:25:33 +0800

    change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,15 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
 
+-- 普通模式下 Ctrl-s 保存
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+
+-- 插入模式下 Ctrl-s 保存（退出再进入插入）
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+
+-- 可视模式下 Ctrl-s 保存（保持选区）
+vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>gv", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
