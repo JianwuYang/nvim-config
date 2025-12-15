91b2aa9a9d1a2fc37da05f1f5af2f5aa582d5070
yangjianwu <yangjianwu@hotmail.com>
2025-12-15 10:23:12 +0800

    change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,6 +11,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.notify("格式化完成", vim.log.levels.INFO)
     end, { buffer = event.buf, desc = "LSP: Format current buffer" })
 
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+    map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+    map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+    map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+    map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+    map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+    map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+
+    map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+
+    map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+
+
     vim.api.nvim_create_user_command(
       "LspInfo",
       ":checkhealth vim.lsp",
